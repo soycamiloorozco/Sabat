9074ac1ff3749909db05c0caeb941242ba246126
Camilo Orozco <camilo.orozco@xpandgroup.co>
2026-05-07 07:34:02 -0500

    Initial Commit

---
+++ Sabat/SabatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SabatApp.swift
+//  Sabat
+//
+//  Created by COrozco on 7/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SabatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
